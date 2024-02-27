d8d159f8d5d8cdbe01894554d8b2ef3643463377
Nina <nina.dong4764@gmail.com>
2024-02-26 21:40:40 -0800

    sync

---
+++ vis/scripts/cameraNavigation.js
@@ -0,0 +1,11 @@
+function fadeOut() {
+  aframe.style.transition="all 1.5s ease-in-out";
+  aframe.style.filter = "brightness(0%)";
+}
+
+function fadeIn() {
+  aframe.style.transition="all 1.5s ease-in-out";
+  aframe.style.filter = "brightness(100%)";
+}
+
+export { fadeOut, fadeIn };

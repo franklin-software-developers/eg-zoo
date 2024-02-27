6cad65e56fbaa5375da26ff05b12846ab99b27d5
Nina <nina.dong4764@gmail.com>
2024-02-26 20:53:36 -0800

    added comments and briefly cleaned code

--- vis/scripts/cameraNavigation.js
+++ vis/scripts/cameraNavigation.js
@@ -2,7 +2,6 @@ import { fadeOut, fadeIn } from  "./fade.js";
 import { animals } from "../../vis/data/cameraData.js";
 
 let aframe = document.getElementById("aframe");
-//fade out
 aframe.addEventListener("transitionend", fadeIn);
 let camera = document.getElementById("camera");
 let animalIndexName = document.getElementById("navigationIndex");
@@ -13,30 +12,30 @@ let rightNavigationButton = document.getElementById("rightNavigationButton");
 leftNavigationButton.addEventListener("click", navigateLeft);
 rightNavigationButton.addEventListener("click", navigateRight);
 
-  function updateIndex () {
-    console.log(animals[index].cameraX + ", " + animals[index].cameraY + ", " + animals[index].cameraZ);
-    animalIndexName.innerHTML = animals[index].name;
-    camera.object3D.position.set(animals[index].cameraX, animals[index].cameraY, animals[index].cameraZ);
+//shows camera name based on current index
+function updateIndex () {
+  animalIndexName.innerHTML = animals[index].name;
+  camera.object3D.position.set(animals[index].cameraX, animals[index].cameraY, animals[index].cameraZ);
+}
+//moves camera to the next coordinate listed. After last coordinate, camera loops to the first coordinate making it an infinite carousel loop.
+function navigateRight() {
+  fadeOut()
+  if (index < animals.length-1) {
+    index++;
+    updateIndex();
+  } else {
+    index = 0;
+    updateIndex();
   }
-  //moves camera to the next coordinate listed. After last coordinate, camera loops to the first coordinate making it an infinite carousel loop.
-  function navigateRight() {
-    fadeOut()
-    if (index < animals.length-1) {
-      index++;
-      updateIndex();
-    } else {
-      index = 0;
-      updateIndex();
-    }
+}
+////moves camera to the previous coordinate listed. After first coordinate, camera loops to the last coordinate making it an infinite carousel loop.
+function navigateLeft() {
+  fadeOut();
+  if (index > 0) {
+    index--;
+    updateIndex();
+  } else {
+    index = animals.length-1;
+    updateIndex();
   }
-  ////moves camera to the previous coordinate listed. After first coordinate, camera loops to the last coordinate making it an infinite carousel loop.
-  function navigateLeft() {
-    fadeOut();
-    if (index > 0) {
-      index--;
-      updateIndex();
-    } else {
-      index = animals.length-1;
-      updateIndex();
-    }
-  }
+}

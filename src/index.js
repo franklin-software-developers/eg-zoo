6ca1bb3050c2254ab336d9f3f463a1378220bbaf
nurikimchi <nurikimchi@gmail.com>
2024-03-14 11:34:05 -0700

    INCOMPLETE WEBPACK SOLUTION

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import '../vis/cameraNavigation' // Camera script
+import '../vis/styles/nav.css'
+import '../vis/savannah/assets/adultGiraffeTextured.glb'
+import '../vis/savannah/assets/savannah.glb'
+import '../vis/savannah/assets/sky.jpg'
+
+
+console.log("Hello World!");
+
+// require('../vis/cameraData');
+// require('../vis/cameraNavigation');
+// require('../vis/fade');
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './vis/savannah/index.html',
+            filename: 'index.html',
+            scriptType: 'module'
+        }),
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [stylesHandler,'css-loader'],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(glb|gltf)$/i,
+                type: 'asset/resource'
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        config.plugins.push(new MiniCssExtractPlugin());
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
